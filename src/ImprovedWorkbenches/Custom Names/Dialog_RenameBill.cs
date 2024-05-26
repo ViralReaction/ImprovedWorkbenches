e44ebfd322150f191900989d680de8ab90b36e2a
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-05-26 13:08:15 +0200

    Update to 1.5

--- src/ImprovedWorkbenches/Custom Names/Dialog_RenameBill.cs
+++ src/ImprovedWorkbenches/Custom Names/Dialog_RenameBill.cs
@@ -2,28 +2,38 @@
 
 namespace ImprovedWorkbenches
 {
-    public class Dialog_RenameBill : Dialog_Rename
+    public class Dialog_RenameBill : Dialog_Rename<IRenameable>
     {
         private readonly ExtendedBillData _extendedBill;
 
-        private readonly string _currentName;
+        //private readonly string _currentName;
 
-        public Dialog_RenameBill(ExtendedBillData extendedBill, string currentName)
+        //public Dialog_RenameBill(ExtendedBillData extendedBill, string currentName)
+        //{
+        //    _extendedBill = extendedBill;
+        //    _currentName = currentName;
+        //    curName = currentName;
+        //}
+
+        public Dialog_RenameBill(ExtendedBillData extendedBill) : base(null)
         {
             _extendedBill = extendedBill;
-            _currentName = currentName;
-            curName = currentName;
         }
 
-        public override void SetName(string name)
+        protected override void OnRenamed(string name)
         {
-            if (string.IsNullOrEmpty(name) || name != _currentName)
-                _extendedBill.Name = name;
+            _extendedBill.Name = name;
         }
 
-        public override AcceptanceReport NameIsValid(string name)
-        {
-            return true;
-        }
+        //public override void SetName(string name)
+        //{
+        //    if (string.IsNullOrEmpty(name) || name != _currentName)
+        //        _extendedBill.Name = name;
+        //}
+
+        //public override AcceptanceReport NameIsValid(string name)
+        //{
+        //    return true;
+        //}
     }
 }
